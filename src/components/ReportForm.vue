73dc881a407e8d06863835865cb006dd5a32a41f
Tree Casiano <tree.pdx@gmail.com>
2019-10-27 16:25:35 -0700

    Make report form intro text more concise

--- src/components/ReportForm.vue
+++ src/components/ReportForm.vue
@@ -7,19 +7,11 @@
           <p>If you see an invasive species while you are in the park, please submit a quick report. We will use the geolocation feature in your browser to get your current location and send the coordinates along with your report submission so we know exactly where you saw the organism.</p>
           <p>If you are not currently near the location where you spotted the invasive species or you do not have a geolocation service enabled, you will need to input the latitude and longitude manually.</p>
           <p>
-            For more information visit:
-            <v-list dense>
-              <v-list-item>
-                <v-list-item-subtitle>
-                  <a href="https://www.portlandoregon.gov/bes/55085">Invasive Animals</a>
-                </v-list-item-subtitle>
-              </v-list-item>
-              <v-list-item>
-                <v-list-item-subtitle>
-                  <a href="https://www.portlandoregon.gov/parks/article/200906">Invasive Plants</a>
-                </v-list-item-subtitle>
-              </v-list-item>
-            </v-list>
+            For more information visit
+            <a
+              href="https://www.portlandoregon.gov/bes/55085"
+            >Invasive Animals</a> and
+            <a href="https://www.portlandoregon.gov/parks/article/200906">Invasive Plants</a>.
           </p>
           <div class="d-flex justify-center">
             <v-btn rounded color="primary" dark v-on="on">
